78227e4faca6d6ea668def801c9cc5e703e4e923
deepika joshi <upreti.deepika@gmail.com>
2023-03-08 13:38:06 +0800

    added reviews model

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_08_051353) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_08_052612) do
   create_table "restaurants", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.integer "phone_number"
-    t.string "category"
+    t.string "name", null: false
+    t.string "address", null: false
+    t.integer "phone_number", null: false
+    t.string "category", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "content", null: false
+    t.integer "rating", null:f alse
+    t.integer "restaurant_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
+  end
+
+  add_foreign_key "reviews", "restaurants"
 end
